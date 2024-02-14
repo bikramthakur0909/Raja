97f733712f4bb29f0f60d13fcfad12ceed193722
Bikram Thakur <bikramthakur0909@gmail.com>
2024-02-15 00:42:03 +0530

    Practice c++

---
+++ demo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int L;
+    cout<<"Enter a length : ";
+    cin>>L;
+    int B;
+    cout<<"Enter a breadth : ";
+    cin>>B;
+    int A = L*B;
+    cout<<"Area of rectangular is "<<A;
+
+}
